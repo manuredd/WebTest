df17f4503edd7a24671dd276d58edd67a1b80ad1
manuredd <manuredd10@gmail.com>
2022-04-10 19:45:23 -0600

    hie

--- kinghouser/index.js
+++ kinghouser/index.js
@@ -21,21 +21,7 @@ class board {
 
 }
 
-function createBoard() {
-    const canvas = document.querySelector("canvas")
-    const boxHeight = window.innerHeight - 10
-    canvas.height = boxHeight
-    canvas.width = boxHeight / 2
-    const boxWidth = boxHeight / 2
-    const squareDim = boxHeight / 7
-    const context = canvas.getContext("2d")
-    for (let i = 0; i < 5; i++) {
-        context.strokeRect(1, (i * boxHeight / 6) + 1, squareDim, squareDim)
-    }
-    for (let i = 0; i < 5; i++) {
-        context.strokeRect(boxWidth / 2, (i * boxHeight / 6) + 1, squareDim, squareDim)
-    }
-}
+
 
 function fillLetters() {
     const canvas = document.querySelector("canvas")
