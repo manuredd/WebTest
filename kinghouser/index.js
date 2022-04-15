b33a6c38fa3ff2bb3969f88a3bcc6bbde72a1308
manuredd <manuredd10@gmail.com>
2022-04-15 11:00:20 -0600

    Changed indexjs file and added fill letter

--- kinghouser/index.js
+++ kinghouser/index.js
@@ -1,34 +1,31 @@
-class board {
-    constructor(canvas, context, boxHeight, boxWidth, squareDim) {
-        this.canvas = canvas
-        canvas = document.querySelector("canvas")
-        this.context = context
-        context = canvas.getContext("2d")
-        this.boxHeight = window.innerHeight - 10
-        this.boxWidth = boxHeight / 2
-        this.squareDim = boxHeight / 7
-    }
-    createBoard() {
-        canvas.height = boxHeight
-        canvas.width = boxHeight / 2
-        for (let i = 0; i < 5; i++) {
-            context.strokeRect(1, (i * boxHeight / 6) + 1, squareDim, squareDim)
-        }
-        for (let i = 0; i < 5; i++) {
-            context.strokeRect(boxWidth / 2, (i * boxHeight / 6) + 1, squareDim, squareDim)
-        }
-    }
+canvas = document.querySelector("canvas")
+context = canvas.getContext("2d")
+const boxHeight = window.innerHeight - 10
+const boxWidth = boxHeight / 2
+const squareDim = boxHeight / 7
 
+function makeBoard() {
+    canvas.height = boxHeight
+    canvas.width = boxHeight / 2
+    for (let i = 0; i < 5; i++) {
+        context.strokeRect(33, (i * boxHeight / 6) + 1, squareDim, squareDim)
+    }
+    for (let i = 0; i < 5; i++) {
+        context.strokeRect(boxWidth / 2 + 33, (i * boxHeight / 6) + 1, squareDim, squareDim)
+    }
 }
-
-
 
 function fillLetters() {
     const canvas = document.querySelector("canvas")
     const context = canvas.getContext("2d")
     context.fillStyle = "black";
-    context.font = "bold 18px Arial";
-    context.fillText("hello", 50, 50)
+    context.font = "60px sans-serif";
+    for (let i = 0; i < 5; i++) {
+        context.fillText("H", 57, 66 + i * boxHeight / 6)
+    }
+    for (let i = 0; i < 5; i++) {
+        context.fillText("H", 57 + boxWidth / 2, 66 + i * boxHeight / 6)
+    }
 }
-createBoard();
+makeBoard();
 fillLetters();
